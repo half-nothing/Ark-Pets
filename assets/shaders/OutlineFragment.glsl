71b525c77003175f05dc1805f19d038872846acf
Harry Huang <harryhuang2652@qq.com>
2024-11-20 16:35:10 +0800

    feat(shader): optimize seaming

--- assets/shaders/OutlineFragment.glsl
+++ assets/shaders/OutlineFragment.glsl
@@ -14,22 +14,23 @@ uniform ivec2 u_textureSize;    // Required
 uniform float u_alpha;          // Required
 
 const float c_alphaLv0 = 0.1;
-const float c_alphaLv1 = 0.5;
+const float c_alphaLv1 = 0.45;
 const float c_alphaLv2 = 0.9;
-const float c_seamCoef = 0.5;
+const float c_seamCoef = 0.55;
 const float c_outlineOverstate = 10.0;
 
-vec4[8] getSimpleNeighbors(vec2 unitLength) {
-    vec4[8] result;
-    result[0] = texture2D(u_texture, v_texCoords + vec2(unitLength.x, 0.0));
-    result[1] = texture2D(u_texture, v_texCoords - vec2(unitLength.x, 0.0));
-    result[2] = texture2D(u_texture, v_texCoords + vec2(0.0, unitLength.y));
-    result[3] = texture2D(u_texture, v_texCoords - vec2(0.0, unitLength.y));
-    result[4] = texture2D(u_texture, v_texCoords + unitLength);
-    result[5] = texture2D(u_texture, v_texCoords - unitLength);
-    result[6] = texture2D(u_texture, v_texCoords + vec2(unitLength.x, -unitLength.y));
-    result[7] = texture2D(u_texture, v_texCoords - vec2(unitLength.x, -unitLength.y));
-    return result;
+vec4[24] getSimpleNeighbors(vec2 unitLength) {
+    vec4 neighbors[24];
+    int i = 0;
+    for (int y = -2; y <= 2; y++) {
+        for (int x = -2; x <= 2; x++) {
+            if (y != 0 && x != 0) {
+                vec2 offset = vec2(x, y) * unitLength;
+                neighbors[i] = texture2D(u_texture, v_texCoords + offset);
+            }
+        }
+    }
+    return neighbors;
 }
 
 vec4[24] getGaussianNeighbors(vec2 unitLength) {
@@ -79,7 +80,7 @@ vec4 getOutlined() {
 vec4 getSeamed() {
     vec4 texColor = texture2D(u_texture, v_texCoords);
     vec2 relPixelSize = vec2(1.0) / u_textureSize;
-    vec4[8] neighbors = getSimpleNeighbors(relPixelSize);
+    vec4[24] neighbors = getSimpleNeighbors(relPixelSize);
     vec4 sampleColor = vec4(0.0);
     int sampleSize = 0;
     for (int i = 0; i < neighbors.length(); i++) {
@@ -90,7 +91,9 @@ vec4 getSeamed() {
     }
     if (sampleSize > 0) {
         texColor.rgb = sampleColor.rgb / sampleSize * c_seamCoef + texColor.rgb * (1.0 - c_seamCoef);
-        texColor.a = sampleColor.a / sampleSize;
+        texColor.a = sampleSize / sampleSize;
+    } else {
+        texColor.a = c_alphaLv2;
     }
     return texColor;
 }
